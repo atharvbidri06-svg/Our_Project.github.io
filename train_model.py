75bfa56252d0fa654ba53a297f821db38dd01a7d
atharvbidri06-svg <atharvbidri06@gmail.com>
2026-04-01 10:03:44 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,1 +1,34 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+import pickle
 
+# Sample dataset (you can replace with Kaggle dataset later)
+data = {
+    "text": [
+        "Government confirms new policy",
+        "Aliens landed in India yesterday",
+        "Stock market hits new high",
+        "Fake miracle cure for cancer discovered",
+        "New education reforms announced"
+    ],
+    "label": [1, 0, 1, 0, 1]  # 1 = Real, 0 = Fake
+}
+
+df = pd.DataFrame(data)
+
+X = df['text']
+y = df['label']
+
+vectorizer = TfidfVectorizer()
+X_vec = vectorizer.fit_transform(X)
+
+model = LogisticRegression()
+model.fit(X_vec, y)
+
+# Save files
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+
+print("Model trained and saved!")
