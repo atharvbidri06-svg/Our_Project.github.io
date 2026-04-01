8ec6bd0f193c54e9b4f4641e5cac2112f409dbd0
atharvbidri06-svg <atharvbidri06@gmail.com>
2026-04-01 10:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
 
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    prediction = ""
+
+    if request.method == "POST":
+        news = request.form["news"]
+        vec = vectorizer.transform([news])
+        result = model.predict(vec)
+
+        if result[0] == 1:
+            prediction = "Real News ✅"
+        else:
+            prediction = "Fake News ❌"
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
